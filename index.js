06b7dfb3b62c858fc429c99fd89f58edc45e9110
adrioro <artistsvillage@me.com>
2019-01-31 17:14:09 -0500

    Third commit

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ document.write("<br/>Here is the array:<br/>");
 for (index in splitIt) {
     document.write(splitIt[index] + ",");
 }
+
+var myString = "My name is Adriana Cerrotti. I am a UX designer."
+var splitIt = myString.split(" ");
+document.write("<br/>Here is the array:<br/>");
+for (index in splitIt) {
+    document.write(splitIt[index] + ", <br/>");
+}
