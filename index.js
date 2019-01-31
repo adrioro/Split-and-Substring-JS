ab5eac76bff7eae06327718a75714ba4fc612bd2
adrioro <artistsvillage@me.com>
2019-01-31 17:21:18 -0500

    Fourth commit

--- index.js
+++ index.js
@@ -18,3 +18,10 @@ document.write("<br/>Here is the array:<br/>");
 for (index in splitIt) {
     document.write(splitIt[index] + ", <br/>");
 }
+
+var myString = "My name is Adriana Cerrotti. I am a UX designer."
+var splitIt = myString.split("", 7); //It'll split only 4 spaces.
+document.write("<br/>Here is the array:<br/>");
+for (index in splitIt) {
+    document.write(splitIt[index] + ", <br/>");
+}
